9c49b8e52072f0381db26b69f197911df9e22a21
Vineee75 <viniciusdev75@gmail.com>
2024-09-02 08:18:32 -0300

    Ajustando importação dos scripts para funcionar melhor em ambiente local

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-import './mascarar-entradas.js';
-import './calcular-regra.js';
-import './func-download.js';
-import './mostrar-menu.js';
-import './mostrar-modal.js';
-import './mostrar-telefone.js';
